1ec80600e12a40bd127d0396bc206cead8fe01c3
JuanMartinMena <juanmartinmenac@gmail.com>
2024-08-07 14:45:00 +0000

    binary_tree_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	parent = node->parent;
 	if (parent->left == node)
-		return parent->right;
+		return (parent->right);
 	else if (parent->right == node)
 		return (parent->left);
 	return (NULL);
